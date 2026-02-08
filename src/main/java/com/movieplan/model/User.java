97439552e05f828c0ac55627130ff85c1d2d24a9
soumya-debug <soumya@182129>
2026-02-08 15:29:12 +0530

    implemented-lombok-throughout-the-app

--- src/main/java/com/movieplan/model/User.java
+++ src/main/java/com/movieplan/model/User.java
@@ -1,81 +1,26 @@
 package com.movieplan.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-	@Column(nullable = false, unique = true, length = 45)
-	private String email;
-	@Column(nullable = false, length = 64)
-	private String password;
-	@Column(nullable = false, length = 20)
-	private String firstName;
-	@Column(nullable = false, length = 20)
-	private String lastName;
-	@Column(nullable = false, length = 20)
-	private String role;
-
-	public User() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+    @Column(nullable = false, length = 64)
+    private String password;
+    @Column(nullable = false, length = 20)
+    private String firstName;
+    @Column(nullable = false, length = 20)
+    private String lastName;
+    @Column(nullable = false, length = 20)
+    private String role;
 }
